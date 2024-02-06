520836f55645260d0f5ae5f82fdcbb243140bc1d
developermartins <martssilvapro@gmail.com>
2024-02-06 16:44:38 -0300

    UserButton SignOut added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
     <main>
-      <h1>Threads</h1>
+      <UserButton afterSignOutUrl="/" />
     </main>
   );
-}
+};
