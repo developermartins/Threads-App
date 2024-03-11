7656abde233fbf7fdf43e927368d9efbf892298d
developermartins <martssilvapro@gmail.com>
2024-03-11 16:15:53 -0300

    setup project

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+async function page() {
+  return (
+    <main>
+      <h1 className='head-text'>Onboarding</h1>
+    </main>
+  );
+};
+
+export default page;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main >
+      <h1>Threads app</h1>
+    </main>
+  );
+}
